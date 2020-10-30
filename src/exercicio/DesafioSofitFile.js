c2184aa05d66cca4ec4761338857f4cf3fed4080
Raphael Costa <rwilliam.sc@gmail.com>
2020-10-30 19:07:53 -0300

    Refatoracao de codigo

--- src/exercicio/DesafioSofitFile.js
+++ src/exercicio/DesafioSofitFile.js
@@ -1,38 +1,11 @@
-import Controller from '../services/Constroller'
+import DesafioSofit from './DesafioSofit'
 import fs from 'fs'
 import path from 'path'
 
-export default class DesafioSofitFile extends Controller {
+export default class DesafioSofitFile extends DesafioSofit {
   constructor (app) {
-    super()
+    super(app)
     this.datasource = app.datasource
-  }
-
-  /** Retorna um array com as datas de alteração do preço do combustível. */
-  async getPrices () {
-    const prices = await this.rest('/data/5f997281c6a3840014e38249/prices')
-
-    return prices.data
-  }
-
-  /** Retorna um array com datas e abastecimentos do veículo em reais (não em litros).. */
-  async getSupplies () {
-    const supplies = await this.rest('/data/5f997281c6a3840014e38249/supplies')
-
-    return supplies.data
-  }
-
-  /** Retorna um array com datas e uso do veículo em quilômetros (quilometragem percorrida no dia). */
-  async getSpents () {
-    const spents = await this.rest('/data/5f997281c6a3840014e38249/spents')
-
-    return spents.data
-  }
-
-  async postValidar (data) {
-    const response = await this.rest.post('/check?id=5f997281c6a3840014e38249', data)
-
-    return response.data
   }
 
   //* * Gerando arquivos para dar outra opção de consumo de dados */
